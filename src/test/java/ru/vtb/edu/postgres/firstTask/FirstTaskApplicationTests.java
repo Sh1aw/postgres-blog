9cb4cdcbc83f5e77f01d16548c9410d0e1295af1
Viktor <victorpetrenko97@gmail.com>
2025-09-04 20:42:14 +0400

    Init blog app

---
+++ src/test/java/ru/vtb/edu/postgres/firstTask/FirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.edu.postgres.firstTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
